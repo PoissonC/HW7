8952063522d054df1d43d37bb34a374bc36a6c0f
PoissonC <95998603+PoissonC@users.noreply.github.com>
2022-04-30 00:01:41 +0800

    Update main.cpp

--- 3D accelerators with FIR low-pass filter/main.cpp
+++ 3D accelerators with FIR low-pass filter/main.cpp
@@ -1,1 +1,280 @@
+/* ----------------------------------------------------------------------
+ * Copyright (C) 2010-2012 ARM Limited. All rights reserved.
+ *
+* $Date:         17. January 2013
+* $Revision:     V1.4.0
+*
+* Project:       CMSIS DSP Library
+ * Title:        arm_fir_example_f32.c
+ *
+ * Description:  Example code demonstrating how an FIR filter can be used
+ *               as a low pass filter.
+ *
+ * Target Processor: Cortex-M4/Cortex-M3
+ *
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*   - Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   - Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in
+*     the documentation and/or other materials provided with the
+*     distribution.
+*   - Neither the name of ARM LIMITED nor the names of its contributors
+*     may be used to endorse or promote products derived from this
+*     software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+ * -------------------------------------------------------------------- */
 
+/**
+ * @ingroup groupExamples
+ */
+
+/**
+ * @defgroup FIRLPF FIR Lowpass Filter Example
+ *
+ * \par Description:
+ * \par
+ * Removes high frequency signal components from the input using an FIR lowpass filter.
+ * The example demonstrates how to configure an FIR filter and then pass data through
+ * it in a block-by-block fashion.
+ * \image html FIRLPF_signalflow.gif
+ *
+ * \par Algorithm:
+ * \par
+ * The input signal is a sum of two sine waves:  1 kHz and 15 kHz.
+ * This is processed by an FIR lowpass filter with cutoff frequency 6 kHz.
+ * The lowpass filter eliminates the 15 kHz signal leaving only the 1 kHz sine wave at the output.
+ * \par
+ * The lowpass filter was designed using MATLAB with a sample rate of 48 kHz and
+ * a length of 29 points.
+ * The MATLAB code to generate the filter coefficients is shown below:
+ * <pre>
+ *     h = fir1(28, 6/24);
+ * </pre>
+ * The first argument is the "order" of the filter and is always one less than the desired length.
+ * The second argument is the normalized cutoff frequency.  This is in the range 0 (DC) to 1.0 (Nyquist).
+ * A 6 kHz cutoff with a Nyquist frequency of 24 kHz lies at a normalized frequency of 6/24 = 0.25.
+ * The CMSIS FIR filter function requires the coefficients to be in time reversed order.
+ * <pre>
+ *     fliplr(h)
+ * </pre>
+ * The resulting filter coefficients and are shown below.
+ * Note that the filter is symmetric (a property of linear phase FIR filters)
+ * and the point of symmetry is sample 14.  Thus the filter will have a delay of
+ * 14 samples for all frequencies.
+ * \par
+ * \image html FIRLPF_coeffs.gif
+ * \par
+ * The frequency response of the filter is shown next.
+ * The passband gain of the filter is 1.0 and it reaches 0.5 at the cutoff frequency 6 kHz.
+ * \par
+ * \image html FIRLPF_response.gif
+ * \par
+ * The input signal is shown below.
+ * The left hand side shows the signal in the time domain while the right hand side is a frequency domain representation.
+ * The two sine wave components can be clearly seen.
+ * \par
+ * \image html FIRLPF_input.gif
+ * \par
+ * The output of the filter is shown below.  The 15 kHz component has been eliminated.
+ * \par
+ * \image html FIRLPF_output.gif
+ *
+ * \par Variables Description:
+ * \par
+ * \li \c testInput_f32_1kHz_15kHz points to the input data
+ * \li \c refOutput points to the reference output data
+ * \li \c testOutput points to the test output data
+ * \li \c firStateF32 points to state buffer
+ * \li \c firCoeffs32 points to coefficient buffer
+ * \li \c blockSize number of samples processed at a time
+ * \li \c numBlocks number of frames
+ *
+ * \par CMSIS DSP Software Library Functions Used:
+ * \par
+ * - arm_fir_init_f32()
+ * - arm_fir_f32()
+ *
+ * <b> Refer  </b>
+ * \link arm_fir_example_f32.c \endlink
+ *
+ */
+
+
+/** \example arm_fir_example_f32.c
+ */
+
+/* ----------------------------------------------------------------------
+** Include Files
+** ------------------------------------------------------------------- */
+#include "mbed.h"
+#include "arm_math.h"
+#include "math_helper.h"
+
+#if defined(SEMIHOSTING)
+#endif
+// Sensors drivers present in the BSP library
+
+#include "stm32l475e_iot01_accelero.h"
+/* ----------------------------------------------------------------------
+** Macro Defines
+** ------------------------------------------------------------------- */
+
+#define TEST_LENGTH_SAMPLES  160
+/*
+This SNR is a bit small. Need to understand why
+this example is not giving better SNR ...
+*/
+#define SNR_THRESHOLD_F32    75.0f
+#define BLOCK_SIZE            32
+
+#if defined(ARM_MATH_MVEF) && !defined(ARM_MATH_AUTOVECTORIZE)
+/* Must be a multiple of 16 */
+#define NUM_TAPS_ARRAY_SIZE              32
+#else
+#define NUM_TAPS_ARRAY_SIZE              29
+#endif
+
+#define NUM_TAPS              29
+
+/* -------------------------------------------------------------------
+ * The input signal and reference output (computed with MATLAB)
+ * are defined externally in arm_fir_lpf_data.c.
+ * ------------------------------------------------------------------- */
+
+extern float32_t testInput_f32_1kHz_15kHz[TEST_LENGTH_SAMPLES];
+extern float32_t refOutput[TEST_LENGTH_SAMPLES];
+
+/* -------------------------------------------------------------------
+ * Declare Test output buffer
+ * ------------------------------------------------------------------- */
+
+static float32_t testOutput_x[TEST_LENGTH_SAMPLES];
+static float32_t testOutput_y[TEST_LENGTH_SAMPLES];
+static float32_t testOutput_z[TEST_LENGTH_SAMPLES];
+/* -------------------------------------------------------------------
+ * Declare State buffer of size (numTaps + blockSize - 1)
+ * ------------------------------------------------------------------- */
+#if defined(ARM_MATH_MVEF) && !defined(ARM_MATH_AUTOVECTORIZE)
+static float32_t firStateF32[2 * BLOCK_SIZE + NUM_TAPS - 1];
+#else
+static float32_t firStateF32[BLOCK_SIZE + NUM_TAPS - 1];
+#endif 
+
+/* ----------------------------------------------------------------------
+** FIR Coefficients buffer generated using fir1() MATLAB function.
+** fir1(28, 6/24)
+** ------------------------------------------------------------------- */
+#if defined(ARM_MATH_MVEF) && !defined(ARM_MATH_AUTOVECTORIZE)
+const float32_t firCoeffs32[NUM_TAPS_ARRAY_SIZE] = {
+  -0.0018225230f, -0.0015879294f, +0.0000000000f, +0.0036977508f, +0.0080754303f, +0.0085302217f, -0.0000000000f, -0.0173976984f,
+  -0.0341458607f, -0.0333591565f, +0.0000000000f, +0.0676308395f, +0.1522061835f, +0.2229246956f, +0.2504960933f, +0.2229246956f,
+  +0.1522061835f, +0.0676308395f, +0.0000000000f, -0.0333591565f, -0.0341458607f, -0.0173976984f, -0.0000000000f, +0.0085302217f,
+  +0.0080754303f, +0.0036977508f, +0.0000000000f, -0.0015879294f, -0.0018225230f, 0.0f,0.0f,0.0f
+};
+#else
+const float32_t firCoeffs32[NUM_TAPS_ARRAY_SIZE] = {
+  -0.0018225230f, -0.0015879294f, +0.0000000000f, +0.0036977508f, +0.0080754303f, +0.0085302217f, -0.0000000000f, -0.0173976984f,
+  -0.0341458607f, -0.0333591565f, +0.0000000000f, +0.0676308395f, +0.1522061835f, +0.2229246956f, +0.2504960933f, +0.2229246956f,
+  +0.1522061835f, +0.0676308395f, +0.0000000000f, -0.0333591565f, -0.0341458607f, -0.0173976984f, -0.0000000000f, +0.0085302217f,
+  +0.0080754303f, +0.0036977508f, +0.0000000000f, -0.0015879294f, -0.0018225230f
+};
+#endif
+
+/* ------------------------------------------------------------------
+ * Global variables for FIR LPF Example
+ * ------------------------------------------------------------------- */
+
+uint32_t blockSize = BLOCK_SIZE;
+uint32_t numBlocks = TEST_LENGTH_SAMPLES/BLOCK_SIZE;
+
+/* ----------------------------------------------------------------------
+ * FIR LPF Example
+ * ------------------------------------------------------------------- */
+
+int32_t main(void)
+{
+  uint32_t i;
+  arm_fir_instance_f32 S1,S2,S3;
+  arm_status status;
+  float32_t  *inputF32_x, *outputF32_x;
+  float32_t  inputF32_y[TEST_LENGTH_SAMPLES]={}, *outputF32_y;
+  float32_t  inputF32_z[TEST_LENGTH_SAMPLES]={}, *outputF32_z;
+  /* Initialize input and output buffer pointers */
+  inputF32_x = &testInput_f32_1kHz_15kHz[0];
+  outputF32_x = &testOutput_x[0];
+  outputF32_y = &testOutput_y[0];
+  outputF32_z = &testOutput_z[0];
+  BSP_ACCELERO_Init();
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      int16_t pfData[3]={0,0,0};
+      BSP_ACCELERO_AccGetXYZ(pfData);
+      wait_us(50);
+      inputF32_x[i]=float(pfData[0]);
+      inputF32_y[i]=float(pfData[1]);
+      inputF32_z[i]=float(pfData[2]);
+  }
+//   for (int i =0;i<TEST_LENGTH_SAMPLES;i++){
+//       printf("x=%f,y=%f,z=%f",inputF32_x[i],inputF32_y[i],inputF32_z[i]);
+//   }
+   /* Call FIR init function to initialize the instance structure. */
+  arm_fir_init_f32(&S1, NUM_TAPS, (float32_t *)&firCoeffs32[0], &firStateF32[0], blockSize);
+  arm_fir_init_f32(&S2, NUM_TAPS, (float32_t *)&firCoeffs32[0], &firStateF32[0], blockSize);
+  arm_fir_init_f32(&S3, NUM_TAPS, (float32_t *)&firCoeffs32[0], &firStateF32[0], blockSize);
+  /* ----------------------------------------------------------------------
+  ** Call the FIR process function for every blockSize samples
+  ** ------------------------------------------------------------------- */
+
+  for(i=0; i < numBlocks; i++)
+  {
+    arm_fir_f32(&S1, inputF32_x + (i * blockSize), outputF32_x + (i * blockSize), blockSize);
+    arm_fir_f32(&S2, inputF32_y + (i * blockSize), outputF32_y + (i * blockSize), blockSize);
+    arm_fir_f32(&S3, inputF32_z + (i * blockSize), outputF32_z + (i * blockSize), blockSize);
+  }
+  //Input data for excel
+  printf("\nInput_x:\n");
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      printf("%f\n",inputF32_x[i]);
+  }
+  printf("\nInput_y:\n");
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      printf("%f\n",inputF32_y[i]);
+  }
+  printf("\nInput_z:\n");
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      printf("%f\n",inputF32_z[i]);
+  }
+  //Input data end
+  //Output data for excel
+  printf("\noutput_x:\n");
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      printf("%f\n",outputF32_x[i]);
+  }
+  printf("\noutput_y:\n");
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      printf("%f\n",outputF32_y[i]);
+  }
+  printf("\noutput_z:\n");
+  for (i=0;i<TEST_LENGTH_SAMPLES;i++){
+      printf("%f\n",outputF32_z[i]);
+  }
+  //Output data end
+
+
+}
+
+/** \endlink */
